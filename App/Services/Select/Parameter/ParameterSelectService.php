48b6e7019a4810f52d2fffb233cebfc6e639b7ff
MohamedHassan1996 <mohamedhassan00096@gmail.com>
2025-06-11 19:40:00 +0300

    update product roleProduct

--- App/Services/Select/Parameter/ParameterSelectService.php
+++ App/Services/Select/Parameter/ParameterSelectService.php
@@ -7,7 +7,7 @@ class ParameterSelectService
 {
     public function getAllParameters(int $parameterId)
     {
-        return DB::raw("SELECT id as value, parameter_value as label FROM parameter_values WHERE parameter_id = ?", [$parameterId]);
+        return DB::raw("SELECT id as value, parameter_value as label FROM parameter_values WHERE parameter_id = ? AND deleted_at IS NULL", [$parameterId]);
     }
 
 }
