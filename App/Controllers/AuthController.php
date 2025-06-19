6c3442da3619b9366bc818eef6a8196af6556dc5
MohamedHassan1996 <mohamedhassan00096@gmail.com>
2025-06-19 13:30:12 +0300

    t

--- App/Controllers/AuthController.php
+++ App/Controllers/AuthController.php
@@ -61,11 +61,11 @@ class AuthController extends Controller implements HasMiddleware
 
         $user = DB::raw("SELECT * FROM users WHERE username = ? AND deleted_at IS NULL", [$data['username']]);
 
-        debug($user);
-
         if (!$user || !Hash::check($data['password'], $user[0]->password)) {
             return ApiResponse::error('Invalid username or password');
         }
+
+                debug($user);
 
         $token = JWT::make([
             'sub' => $user[0]->id,
