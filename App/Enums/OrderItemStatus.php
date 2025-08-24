40155303aea8fe78ee45441f604764ae93297c88
MohamedHassan1996 <mohamedhassan00096@gmail.com>
2025-08-24 12:49:19 +0300

    test

--- App/Enums/OrderItemStatus.php
+++ App/Enums/OrderItemStatus.php
@@ -12,7 +12,6 @@ enum OrderItemStatus: int
 
     case DELIVERED = 5;
 
-    case PARTIALLY_CONFIRMED = 4;
     public static function values(): array
     {
         return array_column(self::cases(), 'value');
